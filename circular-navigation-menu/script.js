dc301f65d22a00f9f2102de324c55532591b24bf
Ricardo Martinez <rmartinez@intellicentrics.com>
2022-08-04 12:26:29 -0600

    Lab done

--- circular-navigation-menu/script.js
+++ circular-navigation-menu/script.js
@@ -5,3 +5,16 @@ menuToggle.addEventListener('click', ()=> {
   menu.classList.toggle('active')
   menuToggle.classList.toggle('active')
 })
+
+const list = document.querySelectorAll('li');
+
+function activeLink() {
+  list.forEach(item => {
+    item.classList.remove('active');
+    this.classList.add('active')
+  })
+}
+
+list.forEach(item => {
+  item.addEventListener('click', activeLink)
+})
